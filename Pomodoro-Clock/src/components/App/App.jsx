0ceda5355027a6fa74feb0b59a0879bf5f0fb317
kwitochka <kwitochka@hotmail.co.jp>
2018-08-25 21:08:27 +0300

    Resolves bug with state collision and formats code

--- Pomodoro-Clock/src/components/App/App.jsx
+++ Pomodoro-Clock/src/components/App/App.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Timer from "../Timer/Timer";
 import Button from "../Button/Button";
-import Settings from '../Settings/Settings'
-
+import Settings from "../Settings/Settings";
+import Footer from "../Footer/Footer";
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class App extends Component {
     // this.timerID = 0;
   }
 
-  get initialState () {
+  get initialState() {
     return {
       breakTime: 5,
       sessionTime: 25,
@@ -22,48 +22,25 @@ class App extends Component {
     };
   }
 
-  _increment = (time) => {
-    if (time === this.state.breakTime && time < 60) {
-      this.setState(prevState => ({
-        breakTime: prevState.breakTime + 1
-      }))
-    } else if (time === this.state.sessionTime && time < 60) {
-      this.setState(prevState => ({
-        sessionTime: prevState.sessionTime + 1
-      }))
-    }
-  };
-
-  _decrement = (time) => {
-    if (time === this.state.breakTime && time > 0){
-      this.setState(prevState=>({
-        breakTime: prevState.breakTime-1
-      }))
-    } else if (time === this.state.sessionTime && time > 0){
-      this.setState(prevState => ({
-        sessionTime: prevState.sessionTime - 1
-      }))
-    }
-  };
-  
   _countDown = () => {
-   if (this.state.seconds === 0) {
+    if (this.state.seconds === 0) {
       clearInterval(this.timerID);
       this.setState(prevState => ({
-        session: !prevState.session,
-      }))
+        session: !prevState.session
+      }));
       this._setTimer();
-        
+
       let audio = new Audio();
-      audio.src = 'http://www.funmag.org/wp-content/uploads/2013/11/08-alarm-ringtones.mp3';
+      audio.src =
+        "http://www.funmag.org/wp-content/uploads/2013/11/08-alarm-ringtones.mp3";
       audio.play();
-      }
+    }
     const seconds = this.state.seconds - 1;
     this.setState({
       seconds,
       active: true
-    })
-  }
+    });
+  };
 
   _startPauseTimer = () => {
     this.setState(prevState => ({
@@ -75,96 +52,123 @@ class App extends Component {
     } else {
       clearInterval(this.timer);
     }
-  }
+  };
 
-  _setBreak = (min) => {
-    this.setState({ 
-      breakTime: min,
-    }) 
-  }
-
-  _setSession = (min) => {
-      this.setState({
-      sessionTime: min,
+  _setBreak = min => {
+    this.setState({
+      breakTime: min
     });
-  }
+  };
+
+  _setSession = min => {
+    this.setState({
+      sessionTime: min
+    });
+  };
 
   _setTimer = () => {
-    this.state.session 
-    ? this.setState({
-        seconds: this.state.sessionTime * 60,
-        session: true
-    })
-    : this.setState({
-      seconds: this.state.breakTime * 60,
-      session: false
-    });
-  }
+    this.state.session
+      ? this.setState({
+          seconds: this.state.sessionTime * 60,
+          session: true
+        })
+      : this.setState({
+          seconds: this.state.breakTime * 60,
+          session: false
+        });
+  };
 
-  // _resetAll = () => {
-  //   clearInterval(this.timer);
-  //   this.setState(this.initialState);
-    // this.audio.pause();
-    // this.audio.currentTime = 0;
-  // }
-
-  render(){
-    const {session, seconds, breakTime, sessionTime} = this.state;
+  render() {
+    const { session, seconds, breakTime, sessionTime } = this.state;
     return (
       <div className="App container">
-        <div className='Settings--wrap'>
-        <Settings time = {this.state.breakTime} 
-          increment={()=> this._increment(breakTime)}
-          decrement={() => this._decrement(breakTime)}
-          setTimer = {this._setBreak} 
-          heading = 'Break time' 
-          id='Break--settings' 
-          timeID = 'Break--time' 
-         
-        />
-        <Settings 
-          time = {this.state.sessionTime} 
-            increment={()=> this._increment(sessionTime)}
-            decrement={() => this._decrement(sessionTime)}
-          setTimer={this._setSession} 
-          heading = 'Session time' 
-          id='Session--settings' 
-          timeID='Session--time' 
-          
-        />
+        <div className="Settings--wrap">
+          <Settings
+            time={this.state.breakTime}
+            label="break-label"
+            incrementId="break-increment"
+            decrementId="break-decrement"
+            increment={() => {
+              if (breakTime < 60) {
+                this.setState(prevState => ({
+                  breakTime: prevState.breakTime + 1
+                }));
+              }
+            }}
+            decrement={() => {
+              if (breakTime > 1) {
+                this.setState(prevState => ({
+                  breakTime: prevState.breakTime - 1
+                }));
+              }
+            }}
+            setTimer={this._setBreak}
+            heading="Break time"
+            id="Break--settings"
+            timeID="break-length"
+          />
+          <Settings
+            time={this.state.sessionTime}
+            label="session-label"
+            incrementId="session-increment"
+            decrementId="session-decrement"
+            increment={() => {
+              if (sessionTime < 60) {
+                this.setState(prevState => ({
+                  sessionTime: prevState.sessionTime + 1
+                }));
+              }
+            }}
+            decrement={() => {
+              if (sessionTime > 1) {
+                this.setState(prevState => ({
+                  sessionTime: prevState.sessionTime - 1
+                }));
+              }
+            }}
+            setTimer={this._setSession}
+            heading="Session time"
+            id="Session--settings"
+            timeID="session-length"
+          />
         </div>
         <Timer
-          heading={session ? 'Session' : 'Break'}
+          heading={session ? "Session" : "Break"}
           seconds={seconds}
-          id="time-left"
+          label="timer-label"
+          min="time-left"
         />
-        <div className='Action'>
-          <Button src={this.state.active ? "https://png.icons8.com/windows/35/ffffff/pause.png" : "https://png.icons8.com/windows/35/ffffff/play.png"} 
-            id="start_stop" 
-            alt={this.state.active ? 'Pause button' : 'Play button'} 
-            handleClick={(e) => {
+        <div className="Action">
+          <Button
+            src={
+              this.state.active
+                ? "https://png.icons8.com/windows/35/ffffff/pause.png"
+                : "https://png.icons8.com/windows/35/ffffff/play.png"
+            }
+            id="start_stop"
+            alt={this.state.active ? "Pause button" : "Play button"}
+            handleClick={e => {
               e.preventDefault();
-              if(this.state.seconds === this.initialState.seconds){
+              if (this.state.seconds === this.initialState.seconds) {
                 this._setTimer();
               }
               this._startPauseTimer();
-            } }
+            }}
           />
           <Button
             src="https://png.icons8.com/windows/35/ffffff/synchronize.png"
             id="reset"
-            type = "reset"
-            alt = 'Reset button' 
+            type="reset"
+            alt="Reset button"
             handleClick={() => {
               this.setState(this.initialState);
-            } 
-          }
+            }}
           />
         </div>
+        <Footer />
       </div>
     );
   }
 }
-
 
 export default App;
--- Pomodoro-Clock/src/components/Settings/Settings.jsx
+++ Pomodoro-Clock/src/components/Settings/Settings.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Minutes from '../Minutes/Minutes';
-import Button from '../Button/Button'; 
+import Minutes from "../Minutes/Minutes";
+import Button from "../Button/Button";
 
-const Settings = ({heading, id, increment, decrement, time}) => {
-    return (
-      <div className="Settings" id={id} onReset = {this._handleReset}>
-        <h2 className="Settings--header">{heading}</h2>
-        <div className="Settings--container" >
-          <Button src='https://png.icons8.com/material-outlined/24/ffffff/plus.png' handleClick={increment} alt = "plus" id = "increment" />
-          <Minutes minutes={time} />
-          <Button src='https://png.icons8.com/small/24/ffffff/minus.png' handleClick={decrement} alt = "minus" id = "decrement" />
-        </div>
+const Settings = ({
+  heading,
+  id,
+  increment,
+  decrement,
+  time,
+  label,
+  incrementId,
+  decrementId
+}) => {
+  return (
+    <div className="Settings" id={id} onReset={this._handleReset}>
+      <h2 className="Settings--header" id={label}>
+        {heading}
+      </h2>
+      <div className="Settings--container">
+        <Button
+          src="https://png.icons8.com/material-outlined/24/ffffff/plus.png"
+          handleClick={increment}
+          alt="plus"
+          id={incrementId}
+        />
+        <Minutes minutes={time} />
+        <Button
+          src="https://png.icons8.com/small/24/ffffff/minus.png"
+          handleClick={decrement}
+          alt="minus"
+          id={decrementId}
+        />
       </div>
-  )
+    </div>
+  );
 };
 
 Settings.propTypes = {
@@ -22,6 +43,9 @@ Settings.propTypes = {
   id: PropTypes.string.isRequired,
   increment: PropTypes.func.isRequired,
   decrement: PropTypes.func.isRequired,
+  incrementId: PropTypes.string.isRequired,
+  decrementId: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
 };
 
 export default Settings;
